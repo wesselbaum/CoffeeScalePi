8ef30b38d54125e3cc36179f3c45c8c657434730
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-11-25 14:07:48 +0100

    another try

--- gui/beanWeight.py
+++ gui/beanWeight.py
@@ -5,6 +5,11 @@ beanWeightBox = None
 text = None
 weight = None
 next = None
+weightGValue = ''
+
+
+def updateLabel():
+    weight.set(weightGValue)
 
 
 def getBeanWeight(app, textSize):
@@ -16,13 +21,15 @@ def getBeanWeight(app, textSize):
     text = Text(beanWeightBox, size=textSize,
                 text='Grind the beans and place them with\nthe container on the scale')
     weight = Text(beanWeightBox, size=textSize*2, text="40g")
+    weight.after(50, updateLabel)
     next = Text(beanWeightBox, size=textSize,
                 text="Click the rotation knob to continue")
     return beanWeightBox
 
 
 def updateWeight(weightG):
+    weightGValue = weightG
     # global weight
-    if (weightG != weight.value):
-        print('update weight: ' + str(weightG))
-        weight.value = str(weightG)
+    # if (weightG != weight.value):
+    # print('update weight: ' + str(weightG))
+    # weight.value = str(weightG)
