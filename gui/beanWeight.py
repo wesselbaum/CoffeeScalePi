d4b45ba44cc7d32067359e25db7befed4885aa74
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-11-25 13:51:56 +0100

    debug

--- gui/beanWeight.py
+++ gui/beanWeight.py
@@ -2,20 +2,26 @@ from guizero import App, Text, Box, Picture
 
 
 beanWeightBox = None
-text  = None
-weight  = None
+text = None
+weight = None
 next = None
+
+
 def getBeanWeight(app, textSize):
     global beanWeightBox
     global text
     global weight
     global next
     beanWeightBox = Box(app, visible=False)
-    text = Text(beanWeightBox, size=textSize, text='Grind the beans and place them with\nthe container on the scale')
+    text = Text(beanWeightBox, size=textSize,
+                text='Grind the beans and place them with\nthe container on the scale')
     weight = Text(beanWeightBox, size=textSize*2, text="40g")
-    next = Text(beanWeightBox, size=textSize, text="Click the rotation knob to continue")
+    next = Text(beanWeightBox, size=textSize,
+                text="Click the rotation knob to continue")
     return beanWeightBox
 
-def updateWeight(weightG: int): 
+
+def updateWeight(weightG):
     global weight
+    print('update weight: ' + str(weightG))
     weight.value = str(weightG) + 'g'
