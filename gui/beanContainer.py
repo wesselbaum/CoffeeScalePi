b5d56f6a9b4826d2acbcfd123db9a509939e0d2a
Aleksej Wesselbaum <aleksej.wesselbaum@codecentric.de>
2023-11-26 13:45:41 +0100

    text wrap

--- gui/beanContainer.py
+++ gui/beanContainer.py
@@ -3,5 +3,6 @@ from guizero import App, Text, Box, Picture
 
 def getBeanContainer(app, textSize):
     beanContainerBox = Box(app, visible=False)
-    text = Text(beanContainerBox, size=textSize, text='Place the bean container on scale and click\n Rotation Knob to start')
+    text = Text(beanContainerBox, size=textSize,
+                text='Place the bean container on scale and\nclick Rotation Knob to start')
     return beanContainerBox
